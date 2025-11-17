510c0e24619f87d6bf1d766c16a817ed70922d59
ZFC-0001 <2511393149@qq.com>
2025-11-17 20:44:59 +0800

    Update 202area.cpp

--- 202area.cpp
+++ 202area.cpp
@@ -7,14 +7,18 @@ int main() {
 	if (x > 0 && x < 1) {
 		y = 3 - 2 * x;
 	}
-	if (x >= 1 && x < 5) {
+	else if (x >= 1 && x < 5) {
 		y = 2 / (4 * x) + 1;
 	}
-	if (x >= 5 && x < 10) {
+	else if (x >= 5 && x < 10) {
 		y = x * x;
+	}
+	else{
+		cout<<"未定义"<<endl;
 	}
 	cout << "答案为：" << y << endl;
 	return 0;
 
 
 }
+
